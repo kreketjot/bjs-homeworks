6b13cfc15b5b4ee54762fa0f6a65077792de6576
Andrey Vinokurov <kreketjot@gmail.com>
2021-03-24 14:36:01 +0300

    hw 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,34 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+'use strict'
+
+function getResult(a, b, c){
+  a = +a; b = +b; c = +c;
+  let res = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d === 0) {
+    res[0] = +(-b / (2 * a)).toFixed(2);
+  } else if (d > 0) {
+    res[0] = +((-b + Math.sqrt(d)) / (2 * a)).toFixed(2);
+    res[1] = +((-b - Math.sqrt(d)) / (2 * a)).toFixed(2);
+  }
+
+  return res;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+  if (marks.length === 0) {
+    return 0;
+  } 
+  if (marks.length > 5) {
+    console.log(`Оценок больше 5`);
+    marks.splice(5);
+  }
+
+  return +((marks.reduce( (sum, x) => sum + +x, 0 ) / marks.length).toFixed(2));
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday){
+  if (new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18) {
+    return `Не желаете ли олд-фэшн, ${name}?`;
+  }
+  return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
 }
